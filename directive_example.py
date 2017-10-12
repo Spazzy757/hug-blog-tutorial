bbafb426a253d4a48e2af5fb9257b773961081d1
Spazzy757 <brendankamp757@gmail.com>
2017-10-12 14:48:41 +0200

    Added examples

---
+++ directive_example.py
@@ -0,0 +1,15 @@
+# directive_example.py
+import hug
+
+# Header Directive
+@hug.directive()
+def foobar_header(**kwargs):
+    header = {
+        'Content-Type': 'application/json'
+    }
+    return header
+
+# The Endpoint that uses the directive
+@hug.get("/data")
+def get_data(name, hug_foobar_header):
+    return {"header": hug_foobar_header, "data": {"name": name}}
